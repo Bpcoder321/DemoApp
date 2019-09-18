324865f4d2ee1c411273f37a0edf0825ea144098
bp <manoranjansahoo967@gmal.com>
2019-09-18 23:44:22 +0530

    initial commit of experiment branch

--- Test.java
+++ Test.java
@@ -2,5 +2,12 @@ class Test
 {
 	public void m1(){
 		System.out.println("hello");
+		System.out.println("hello");
+
+	}
+	public void m2(){
+		System.out.println("hello");
+		System.out.println("hello");
+
 	}
 }
