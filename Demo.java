c455b4a0b7998d892078908e9e2725c16b14906f
bp <manoranjansahoo967@gmal.com>
2019-09-19 00:02:44 +0530

    cchanges in Demo.java

--- Demo.java
+++ Demo.java
@@ -2,8 +2,12 @@ class Demo{
  public static void main(Strig [] args)
  {
    wish();
+   greet();
  }
  public void wish(){
+     System.out.println("gud morning");
+ }
+ public void greet(){
      System.out.println("gud morning");
  }
 }
